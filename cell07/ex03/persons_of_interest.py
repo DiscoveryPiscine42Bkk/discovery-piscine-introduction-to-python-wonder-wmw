5bfdbc400e40bd320725ff01b1eb2a56f96c9a3c
wonder-wmw <ruengrith.cmr@gmail.com>
2025-05-09 14:53:02 +0700

    Update persons_of_interest.py

--- cell07/ex03/persons_of_interest.py
+++ cell07/ex03/persons_of_interest.py
@@ -1,22 +1,4 @@
 #!/usr/bin/python3
 
 
-women_scientists = {
-"ada": { "name": "Ada Lovelace", "date_of_birth": "1815" },
-"cecilia": { "name": "Cecila Payne", "date_of_birth": "1900" },
-"lise": { "name": "Lise Meitner", "date_of_birth": "1878" },
-"grace": { "name": "Grace Hopper", "date_of_birth": "1906" }
-}
-# famous_births(women_scientists)
-
-
-# d = [
-#     {"name": "Alice", "age": 30},
-#     {"name": "Bob", "age": 25},
-#     {"name": "Charlie", "age": 35}
-# ]
-
-# d.sort(key=lambda person: person["age"])
-# print(d)
-
 print(women_scientists["ada"]["name"])
